61592ad66cc7bf4dca5df776ba727f23716530bb
ValeAF1112 <valentina.arciniega2296@alumnos.udg.mx>
2026-03-13 19:50:35 -0600

    Update Arreglo de caracteres.c

--- strings.p/Arreglo de caracteres.c
+++ strings.p/Arreglo de caracteres.c
@@ -8,15 +8,18 @@ int main(){
     char lista[5][50];
     int N = 3;
     int longitud;
+    char name[15], lastname[15], full_name[30];
 
-    /*printf("Ingrese el primer nombre:\n");
+    printf("\tEjemplo funcion fgets:\n");
+    printf("Ingrese el primer nombre: ");
     fgets(nombre1, sizeof(nombre1), stdin);
 
-    printf("Ingrese el segundo nombre:\n");
+    printf("Ingrese el segundo nombre: ");
     fgets(nombre2, sizeof(nombre2), stdin);
-    printf("%s\n", nombre1);
-    printf("%s", nombre2);*/
+    printf("\n%s", nombre1);
+    printf("%s\n", nombre2);
 
+    printf("\tEjemplo funcion fgets pero con lista(para varios nombres) y funcion strlen:\n");
     for(int i = 0; i < N; i ++){
         printf("Ingrese el nombre numero %i:", i + 1);
         fgets(lista[i], sizeof(lista[i]), stdin);
@@ -24,8 +27,25 @@ int main(){
     for(int j = 0; j < N; j ++){
         printf("\n%s", lista[j]);
         longitud = strlen(lista[j]);
-        printf("\nLa longitud del nombre %i de la lista es: %d", j+1, longitud);
+        printf("La longitud del nombre %i de la lista es: %d\n", j+1, longitud);
     }
+
+    printf("\tEjemplo funcion strcpy, strcat y strcomp: \n");
+    printf("Ingrese su nombre: ");
+    fgets(name, sizeof(name), stdin);
+    printf("Ingrese su apellido: ");
+    fgets(lastname, sizeof(lastname), stdin);
+
+    printf("%s\n", name);
+    printf("%s\n", lastname);
+
+    strcpy(full_name, name);
+    strcat(full_name, lastname);
+    int Comp = strcmp(name, lastname);
+
+    printf("%s\n", full_name);
+    printf("%d", Comp);
+
 
     return 0;
 }
